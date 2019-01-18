ba5dcadd3be9c9014ea6c2bc546a53bf1c41f302
NMcDonough <mcnoah94@gmail.com>
2019-01-18 15:15:40 -0500

    Integrated Pandas and d3 4.0 to display histogram data

--- apps/main/static/main/graphing.js
+++ apps/main/static/main/graphing.js
@@ -1,76 +1,46 @@
-function exampleHistogram(values, bins) {
-    var color = "steelblue";
+function exampleHistogram(values) {
+    var data = values;
+    var formatCount = d3.format("");
 
-    // A formatter for counts.
-    var formatCount = d3.format(",.0f");
+    var svg = d3.select("svg"),
+        margin = {top: 10, right: 30, bottom: 30, left: 30},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var margin = {top: 20, right: 30, bottom: 30, left: 30},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    var max = d3.max(values);
-    var min = d3.min(values);
-    var x = d3.scale.linear()
-        .domain([min, max])
+    var x = d3.scaleLinear()
+        .domain([d3.min(data), d3.max(data)])
         .range([0, width]);
-    // Generate a histogram using twenty uniformly-spaced bins.
-    var data = d3.layout.histogram()
-        .bins(bins)
-        (values);
 
-    var yMax = d3.max(data, function(d){return d.length});
-    var yMin = d3.min(data, function(d){return d.length});
-    var colorScale = d3.scale.linear()
-                .domain([yMin, yMax])
-                .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+    var bins = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(20))
+        (data);
 
-    var y = d3.scale.linear()
-        .domain([0, yMax])
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(bins, function(d) { return d.length; })])
         .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([100, 0])
-    .html(function(d) {
-        return "<p>" + d + "</p>";
-    })
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    svg.call(tip);
-
-    var bar = svg.selectAll(".bar")
-        .data(data)
+    var bar = g.selectAll(".bar")
+    .data(bins)
     .enter().append("g")
-        .attr("class", "bar test")
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
-        .on('click', tip.show)
-        .on('mouseout', tip.hide);
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
 
     bar.append("rect")
         .attr("x", 1)
-        .attr("width", (x(data[0].dx) - x(0)) - 1)
-        .attr("height", function(d) { return height - y(d.y); })
-        .attr("fill", function(d) { return colorScale(d.y) });
+        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+        .attr("height", function(d) { return height - y(d.length); });
 
-    // bar.append("text")
-    //     .attr("dy", ".75em")
-    //     .attr("y", -12)
-    //     .attr("x", (x(data[0].dx) - x(0)) / 2)
-    //     .attr("text-anchor", "middle")
-    //     .text(function(d) { return formatCount(d.y); })
-    //     .attr('class', 'test');
+    bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", -15)
+        .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.length); });
 
-    svg.append("g")
-        .attr("class", "x axis")
+    g.append("g")
+        .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(d3.axisBottom(x));
 }
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,11 +1,20 @@
+import pandas as pd
 from django.shortcuts import render, HttpResponse
 from .excelParser import parseExcelFile
-import json
+import json, codecs
+import os as os
 
 def index(req):
     return render(req, 'main/index.html')
 
 def histogram(req):
-    data = parseExcelFile("histogram-data.xlsx", False)
-    print(data)
-    return HttpResponse(json.dumps({'data': data}), content_type="application/json")
+    binCount = 20
+    xlfile = '/Users/noah/Documents/Projects/Python/histogram/apps/main/histogram_data.xlsx'
+    #output_file('output_file_test.html',title='Total Revenue Figure')
+    revenueColumn = 'total_revenue'
+
+    df = pd.read_excel(xlfile, 0)
+    dfRevenue = df[revenueColumn].values
+    print(dfRevenue)
+
+    return HttpResponse(json.dumps({'data': dfRevenue.tolist()}), content_type="application/json")
